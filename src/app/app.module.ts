1473203a94af36a9fbbe746b398d1b9337eec185
Saravanan Shanmugavel <saravanan.shun@gmail.com>
2019-03-19 22:00:46 -0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,35 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PracticeComponent } from './practice/practice.component';
+import {RouterModule, Routes} from "@angular/router";
+
+
+const appRoutes: Routes = [
+  {
+    path: 'practice',
+    component: PracticeComponent,
+    data: { title: 'Code Practice' }
+  },
+  { path: '',
+    redirectTo: '/practice',
+    pathMatch: 'full'
+  }
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PracticeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true }
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
